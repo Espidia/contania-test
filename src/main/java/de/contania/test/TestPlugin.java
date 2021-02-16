3b8533f56b65940edb5a8bb720328ff5f6f02cfd
Espidia <paul.ackermann1006@gmail.com>
2021-02-16 16:36:24 +0100

    Initial commit

---
+++ src/main/java/de/contania/test/TestPlugin.java
@@ -0,0 +1,14 @@
+package de.contania.test;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author Espidia
+ */
+public class TestPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+}
